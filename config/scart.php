0c89d4f45c5ec2bb208323e0c10a94281802dd24
Hoa Doan Quang <doan.quang.hoa@sun-asterisk.com>
2019-07-31 13:09:36 +0700

    first commit

---
+++ config/scart.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'version'  => '2.2.8',
+    'homepage' => 'https://devskill.org',
+    'name'     => 'Shopping Cart',
+    'title'    => 'eCommerce for Business',
+    'github'   => 'https://github.com/lanhktc/s-cart',
+    'email'    => 'lanhktc@gmail.com',
+    'settings' => [
+    ],
+];
